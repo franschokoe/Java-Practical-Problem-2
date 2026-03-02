c1201e1c8912aca27a44e067cf1560cfa2d6d260
Frans Chokoe <fmoyagabo@gmail.com>
2026-03-02 23:01:54 +0200

    Declare Variables

--- src/UserInforConsole.java
+++ src/UserInforConsole.java
@@ -12,5 +12,22 @@ public class UserInforConsole {
 
         //and asking information
 
+        // User first name
+        System.out.print("Enter your first name: ");
+        firstName = scanner.nextLine();
+        // User last name
+        System.out.print("Enter your last name: ");
+        lastName = scanner.nextLine();
+        // User Age
+        System.out.print("Enter your age(yrs): ");
+        age = scanner.nextInt();
+        //user annual salary
+        System.out.print("Enter your annual salary: ");
+        annualSalary = scanner.nextDouble();
+
+
+
+
+
     }
 }
