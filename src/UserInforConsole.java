b50c93812e7e186ac107e0ea195d4dc792f6cc35
Frans Chokoe <fmoyagabo@gmail.com>
2026-03-02 22:48:54 +0200

    innitial commit

---
+++ src/UserInforConsole.java
@@ -0,0 +1,16 @@
+import java.util.Scanner; // we need the scanner to allow input on the terminal
+
+public class UserInforConsole {
+    public static void main(String [] args){
+        //Declaring scanner
+        Scanner scanner = new Scanner(System.in);
+
+        //Declaring variable
+        String firstName , lastName;
+        int age ;
+        double annualSalary , dailySalary ;
+
+        //and asking information
+
+    }
+}
