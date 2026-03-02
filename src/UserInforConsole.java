51098a05a03bc824f43d481e238e64b531887528
Frans Chokoe <fmoyagabo@gmail.com>
2026-03-02 23:17:43 +0200

    Output message

--- src/UserInforConsole.java
+++ src/UserInforConsole.java
@@ -25,6 +25,19 @@ public class UserInforConsole {
         System.out.print("Enter your annual salary: ");
         annualSalary = scanner.nextDouble();
 
+        int ageMonths = age * 12 ;
+        int daysWork = 260;
+        dailySalary = annualSalary/daysWork;
+//        double formattedDailySalary
+        // OUTPUT  OF THE USER INPUT
+
+        System.out.println("Employee Information");
+        System.out.println("Full name: " + firstName + lastName);
+        System.out.println("Age(months): " + ageMonths);
+        System.out.println("Age(yrs): " +age);
+        System.out.println("Daily salary: R %2f%" + dailySalary);
+
+
 
 
 
